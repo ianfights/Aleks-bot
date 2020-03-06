cbf16d1e2210807ab902697e424a88564bf2dec2
Ian Davis <ianfights@gmail.com>
2020-03-06 08:44:57 -0500

    made it work on linux

--- index.py
+++ index.py
@@ -1,60 +1,91 @@
+#!/usr/bin/env python
+#driver.save_screenshot('/home/pi/Desktop/aleks_bot/SeleniumChromiumTest.png')
 from selenium import webdriver
+from pyvirtualdisplay import Display
 from time import sleep
 username = raw_input("Username: ")
 password = raw_input("Password: ")
+#driver = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver')
+#print 'Starting ...'
+display = Display(visible=0, size=(1600, 1200))
+display.start()
+driver = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver')
+print 'webdriver loaded'
 
-class AleksBot():
-    def __init__(self):
-        self.driver = webdriver.Chrome()
 
-    def login(self):
-        self.driver.get('https://connected.mcgraw-hill.com/connected/login.do')
+driver.get('https://connected.mcgraw-hill.com/connected/login.do')
 
-        sleep(2)
+sleep(2)
 
-        email_in = self.driver.find_element_by_xpath('//*[@id="loginUserName"]')
-        email_in.send_keys(username)
+email_in = driver.find_element_by_xpath('//*[@id="loginUserName"]')
+email_in.send_keys(username)
+pw_in = driver.find_element_by_xpath('//*[@id="loginPassword"]')
+pw_in.send_keys(password)
 
-        pw_in = self.driver.find_element_by_xpath('//*[@id="loginPassword"]')
-        pw_in.send_keys(password)
+login_btn = driver.find_element_by_xpath('//*[@id="loginLink"]')
+login_btn.click()
 
-        login_btn = self.driver.find_element_by_xpath('//*[@id="loginLink"]')
-        login_btn.click()
-
-        sleep(15)
-        popup_1 = self.driver.find_element_by_xpath('//*[@id="NG8D8OGC6M5G2Z5C"]/div/div/div[2]/button')
-        popup_1.click()
-        sleep(5)
-        popup_2 = self.driver.find_element_by_xpath('//*[@id="rotatorContent"]/table/tbody/tr/td[1]/div/div/div/a')
-        popup_2.click()
-        sleep(3)
-        popup_3 = self.driver.find_element_by_xpath('//*[@id="lessonSearchResults"]/div/div[3]/div')
-        popup_3.click()
+sleep(15)
+popup_1 = driver.find_element_by_xpath('//*[@id="NG8D8OGC6M5G2Z5C"]/div/div/div[2]/button')
+popup_1.click()
+sleep(5)
+popup_2 = driver.find_element_by_xpath('//*[@id="rotatorContent"]/table/tbody/tr/td[1]/div/div/div/a')
+popup_2.click()
+sleep(3)
+popup_3 = driver.find_element_by_xpath('//*[@id="lessonSearchResults"]/div/div[3]/div')
+popup_3.click()
         
     
-        base_window = self.driver.window_handles[0]
-        self.driver.switch_to_window(self.driver.window_handles[1])
-        sleep(20)
-        try:
-            popup_4 = self.driver.find_element_by_xpath('//*[@id="smt_bottomnav_button_input_assignment_home_auto_db_c2ed66a_quiz#1104_2530"]')
-            popup_4.click()
-        except:
+base_window = driver.window_handles[0]
+driver.switch_to_window(driver.window_handles[1])
+sleep(20)
+
+#//*[@id="smt_hamburgermenu_button_input_start_learning_HB"]
+
+menu = driver.find_element_by_xpath('//*[@id="smt_hbmenu_button_input"]')
+menu.click()
+sleep(1)
+learn = driver.find_element_by_xpath('//*[@id="smt_hamburgermenu_button_input_start_learning_HB"]')
+learn.click()
+driver.save_screenshot('/home/pi/Desktop/aleks_bot/SeleniumChromiumTest.png')
+
+'''try:
+	popup_6 = driver.find_element_by_xpath('//*[@id="current-main-view"]/div[2]/div[3]/button[1]')
+        popup_6.click()
+except:
             
-            popup_5 = self.driver.find_element_by_xpath('//*[@id="smt_bottomnav_button_input_start_learning"]')
-            popup_5.click()
-       
+	popup_5 = driver.find_element_by_xpath('//*[@id="smt_bottomnav_button_input_start_learning"]')
+	popup_5.click()
         sleep(5)
-        
-                    
+ '''          
+    
 
-        while True:
-            sleep(20)
-            popup_8 = self.driver.find_element_by_xpath('//*[@id="smt_bottomnav_button_input_requestExplain"]')
-            popup_8.click()
-            sleep(40)
-            popup_9 = self.driver.find_element_by_xpath('//*[@id="smt_bottomnav_button_input_learning"]')
-            popup_9.click()
+                   
+
+while True:
+    try:
+	    start = driver.find_element_by_xpath('//*[@id="smt_bottomnav_button_input_learning"]')
+            start.click()
+    except:
+        sleep(20)
+	explain = driver.find_element_by_xpath('//*[@id="smt_bottomnav_button_input_requestExplain"]')
+	explain.click()
+    sleep(40)
+  
+    popup_9 = driver.find_element_by_xpath('//*[@id="smt_bottomnav_button_input_learning"]')
+    popup_9.click()
+    
+    
+    sleep(5)
+    
+    '''sleep(20)
+	popup_8 = driver.find_element_by_xpath('//*[@id="smt_bottomnav_button_input_requestExplain"]')
+	popup_8.click()
+       # print('Explain pressed')
+	sleep(40)
+        popup_9 = driver.find_element_by_xpath('//*[@id="smt_bottomnav_button_input_learning"]')
+        popup_9.click()
+           # print('Continue Learning Pressed')
+           '''
         
     
-bot = AleksBot()
-bot.login()
